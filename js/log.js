2cbaeabab8ffaa6374bcf58a193629fdeab33984
Mathieu B <spacedoodleone@gmail.com>
2019-10-16 23:00:16 +0100

    Education

--- js/log.js
+++ js/log.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
    $('.login_panel').hide();
+   $('.register_panel').hide();
+  $("#login").click(function(){
+    $('.login_panel').fadeToggle();
+    $('.register_panel').hide();
+  });
+  $("#register").click(function(){
+    $('.register_panel').fadeToggle();
+    $('.login_panel').hide();
+  });
+
 });
